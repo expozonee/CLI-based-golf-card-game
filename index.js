ef2de31039d19cf3985019b9ba0013ac2643ec98
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-21 10:20:54 +0300

    added the logic function to start and game and display the current status of the board

--- index.js
+++ index.js
@@ -55,3 +55,41 @@ function shuffleDeck(deck) {
 shuffleDeck(deck);
 let discardPile = [];
 let deckIsEmpty = false;
+
+function startGame() {
+  players.forEach((player) => {
+    for (let i = 0; i < 4; i++) {
+      player.cards.push(deck.pop());
+    }
+  });
+  discardPile.push(deck.pop());
+  taketurn(0);
+}
+
+function board() {
+  console.log("   ");
+  console.log("------ Board ------");
+
+  for (const player of players) {
+    let msg = [];
+    player.cards.forEach((card, index) => {
+      if (player.isShown[index]) {
+        msg.push(`[${card.rank} of ${card.suit}]`);
+      } else {
+        msg.push("[Face Down]");
+      }
+    });
+    console.log(`Hand of ${player.name}: `, msg.join(", "));
+  }
+  console.log(
+    `Discard Pile Top Card: ${
+      discardPile.length > 0
+        ? `${discardPile[discardPile.length - 1].rank} of ${
+            discardPile.length > 0 && discardPile[discardPile.length - 1].suit
+          }`
+        : "Empty"
+    } `
+  );
+  console.log("--------------------");
+  console.log("   ");
+}
